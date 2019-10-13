33920bf48fca1b835a23b4351105645f5380de88
Jacob Horrocks <jacob.horrocks@gmail.com>
2019-10-12 20:53:21 -0700

    horrocks-minor-mode: bind layout control commands

--- horrocks/init.d/external/init-winum.el
+++ horrocks/init.d/external/init-winum.el
@@ -1,3 +1,16 @@
 (use-package winum
   :init
   (setq winum-keymap (make-sparse-keymap)))
+
+(customize-setq
+ ;; winum-assign-func
+ ;; winum-assign-functions
+ ;; winum-auto-assign-0-to-minibuffer
+ ;; winum-auto-setup-mode-line
+ ;; winum-format
+ ;; winum-ignored-buffers
+ winum-mode t
+ ;; winum-mode-line-position
+ ;; winum-reverse-frame-list
+ ;; winum-scope
+ )
