2b96263d0b68131142377f90aee677aaf43fc5f6
Jacob Horrocks <jacob.horrocks@gmail.com>
2019-10-13 18:46:57 -0700

    ivy: bind "M-n" to ivy-restrict-to-matches in ivy-minibuffer-map
    This is initially set to "S-SPC", but my terminal doesn't support this key
    combination.

--- horrocks/init.d/external/init-ivy.el
+++ horrocks/init.d/external/init-ivy.el
@@ -8,6 +8,8 @@
    ivy-use-virtual-buffers nil)
   (add-to-list 'ivy-ignore-buffers "\\*epc con")
   :init
-  (ivy-mode))
+  (ivy-mode)
+  :bind (:map ivy-minibuffer-map
+              ("M-n" . ivy-restrict-to-matches)))
 
 (use-package ivy-hydra)
