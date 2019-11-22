59f39d9ac424c4800d047c3baace053e1f913246
Jacob Horrocks <jacob.horrocks@gmail.com>
2019-11-21 22:55:39 -0800

    ediff: customize window behavior

---
+++ horrocks/init.d/builtin/ediff-window.el
@@ -0,0 +1,12 @@
+(customize-setq
+ ;; ediff-control-frame-position-function
+ ;; ediff-control-frame-upward-shift
+ ;; ediff-grab-mouse
+ ;; ediff-merge-split-window-function
+ ;; ediff-narrow-control-frame-leftward-shift
+ ;; ediff-prefer-iconified-control-frame
+ ediff-split-window-function #'split-window-horizontally
+ ;; ediff-use-long-help-message
+ ;; ediff-wide-control-frame-rightward-shift
+ ediff-window-setup-function #'ediff-setup-windows-plain
+ )
