953833e8f0119f102e07c7b6e3c6d32b4b81c312
Jacob Horrocks <jacob.horrocks@gmail.com>
2020-09-02 21:06:54 -0700

    hydra-outline: reduce complexity

--- horrocks/lib/hydra-outline.el
+++ horrocks/lib/hydra-outline.el
@@ -1,36 +1,22 @@
-(require 'hydra)
-
-(defhydra hydra-outline (:exit nil :foreign-keys warn :hint nil)
+(defhydra hydra-outline (:foreign-keys warn
+                         :hint nil
+                         :inherit (hydra-mixin-quit/heads))
   "
-^Hide^             ^Show^           ^Move
-^^^^^^------------------------------------------------------
-_q_: sublevels     _a_: all         _u_: up
-_t_: body          _e_: entry       _n_: next visible
-_o_: other         _i_: children    _p_: previous visible
-_c_: entry         _k_: branches    _f_: forward same level
-_l_: leaves        _s_: subtree     _b_: backward same level
-_d_: subtree
+  Navigation: [_j_ next] [_k_ previous]]
+  Visibility: [_TAB_ cycle] [_h_ide all] [_s_how all]
+Manipulation: [_H_ drag backward] [_L_ drag forward] [_<_ demote] [_>_ promote]
 
 "
-  ;; Hide
-  ("q" hide-sublevels)    ; Hide everything but the top-level headings
-  ("t" hide-body)         ; Hide everything but headings (all body lines)
-  ("o" hide-other)        ; Hide other branches
-  ("c" hide-entry)        ; Hide this entry's body
-  ("l" hide-leaves)       ; Hide body lines in this entry and sub-entries
-  ("d" hide-subtree)      ; Hide everything in this entry and sub-entries
-  ;; Show
-  ("a" show-all)          ; Show (expand) everything
-  ("e" show-entry)        ; Show this heading's body
-  ("i" show-children)     ; Show this heading's immediate child sub-headings
-  ("k" show-branches)     ; Show all sub-headings under this heading
-  ("s" show-subtree)      ; Show (expand) everything in this heading & below
-  ;; Move
-  ("u" outline-up-heading)                ; Up
-  ("n" outline-next-visible-heading)      ; Next
-  ("p" outline-previous-visible-heading)  ; Previous
-  ("f" outline-forward-same-level)        ; Forward - same level
-  ("b" outline-backward-same-level)       ; Backward - same level
-  ("z" nil "leave"))
+  ;; Navigation
+  ("k" outline-previous-visible-heading)
+  ("j" outline-next-visible-heading)
 
-(provide 'hydra-outline)
+  ;; Visibility
+  ("TAB" outline-cycle)
+  ("h" outline-hide-body)
+  ("s" outline-show-all)
+
+  ("H" outline-move-subtree-up)
+  ("L" outline-move-subtree-down)
+  ("<" outline-demote)
+  (">" outline-promote))
