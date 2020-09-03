3d56854f657bad4c8e4653786826ab149ed5357f
Jacob Horrocks <jacob.horrocks@gmail.com>
2020-09-03 07:33:00 -0700

    org: add more languages to babel and improve code block formatting

--- horrocks/init.d/builtin/org-babel.el
+++ horrocks/init.d/builtin/org-babel.el
@@ -1,4 +1,14 @@
 (customize-setq
  org-babel-load-languages '((emacs-lisp  . t)
-                            (shell . t))
- )
+                            (python . t)
+                            (shell . t)
+                            (sql . t)
+                            (sqlite . t))
+
+ ;; by default org wants to add additional indentation for code, no thanks.
+ org-edit-src-content-indentation 0
+
+ org-src-fontify-natively t
+
+ ;; Enable code block indenting by its mode
+ org-src-tab-acts-natively t)
