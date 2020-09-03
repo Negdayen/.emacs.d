a06135df2ff2d0f7d69b4f175f26944d04be7e39
Jacob Horrocks <jacob.horrocks@gmail.com>
2020-09-02 21:13:38 -0700

    factor out lisp-family language configuration

---
+++ horrocks/init.d/lang/lisp.el
@@ -0,0 +1,23 @@
+(dolist (hook '(emacs-lisp-mode-hook
+                eval-expression-minibuffer-setup-hook
+                inferior-scheme-mode-hook
+                lisp-mode-hook
+                scheme-mode-hook))
+  (add-hook hook 'prettify-symbols-mode)
+  (add-hook hook 'smartparens-strict-mode)
+  (add-hook
+   hook
+   (lambda ()
+     (setq-local
+      sp-local-pairs
+      '((:open "\\\"" :close "\\\"" :actions (insert wrap autoskip navigate))
+        (:open "\"" :close "\""
+         :actions
+         (insert wrap autoskip navigate escape)
+         :unless
+         (sp-in-string-quotes-p)
+         :post-handlers
+         (sp-escape-wrapped-region sp-escape-quotes-after-insert))
+        (:open "(" :close ")" :actions (insert wrap autoskip navigate))
+        (:open "[" :close "]" :actions (insert wrap autoskip navigate))
+        (:open "{" :close "}" :actions (insert wrap autoskip navigate)))))))
