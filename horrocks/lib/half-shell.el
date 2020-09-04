78d4c826fb0607b7017d9679e8bce51c13926ab9
Jacob Horrocks <jacob.horrocks@gmail.com>
2020-09-04 07:41:00 -0700

    half-shell: add defsh and inline-shell-command

--- horrocks/lib/half-shell.el
+++ horrocks/lib/half-shell.el
@@ -1,5 +1,23 @@
 (defun shell-command-to-list (command)
-  ""
+  "Return output lines of shell COMMAND as list of strings."
   (split-string-and-unquote (shell-command-to-string command) "\n"))
+
+(defun inline-shell-command (command &optional buffer)
+  "Insert output of shell COMMAND stripped of newlines into BUFFER, defaulting
+to the current buffer."
+  (interactive "MShell command: \n")
+  (with-current-buffer (or buffer (current-buffer))
+    (insert
+     (shell-command-to-string
+      (format "%s | tr '\n' ' ' | sed 's| $||'" command)))))
+
+(defmacro defsh (name command)
+  "Define wrapper interactive function NAME executing shell COMMAND on region."
+  `(defun ,name (start end)
+     ,(format "Execute `%s` on region." command)
+     (interactive "r")
+     (let ((default-directory temporary-file-directory)) ;ignore TRAMP remote
+       (shell-command-on-region start end ,command t t))
+     (exchange-point-and-mark)))
 
 (provide 'half-shell)
