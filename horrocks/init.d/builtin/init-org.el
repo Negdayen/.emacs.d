d94ca85bf7fa9f03735adb20b161d2f8d5e30528
Jacob Horrocks <jacob.horrocks@gmail.com>
2020-09-05 15:36:25 -0700

    org: avoid use-package idioms
    Weening off use-package as I find its syntax unnecessary and unintuitive.

--- horrocks/init.d/builtin/init-org.el
+++ horrocks/init.d/builtin/init-org.el
@@ -1,16 +1,24 @@
-(use-package org
-  :pin org
+(use-package org :pin org)
 
-  :init
-  (setq
-   ;; Enable code block fontification by its mode
-   org-src-fontify-natively t
-   ;; Enable code black indenting by its mode
-   org-src-tab-acts-natively t)
+(with-eval-after-load 'org
+  (bind-key "C-c C-v '" 'org-edit-src-code org-mode-map)
+  ;; org-mode sets this to org-{backward,foward}-element, which I don't like.
+  ;; Unbinding sets these to org-{backward,foward}-paragraph instead.
+  (bind-key "M-{" nil org-mode-map)
+  (bind-key "M-}" nil org-mode-map)
 
-  :config
-  ;; cycle lists like headings
+  ;; Cycle lists like headings
   (setq org-cycle-include-plain-lists 'integrate)
+
+  ;; Toggle visibility of all code blocks by hitting C-c C-\
+  (defvar org-blocks-hidden nil)
+  (defun org-toggle-blocks ()
+    (interactive)
+    (if org-blocks-hidden (org-show-block-all) (org-hide-block-all))
+    (setq-local org-blocks-hidden (not org-blocks-hidden)))
+  (define-key org-mode-map (kbd "C-c C-\\") 'org-toggle-blocks)
+
+  ;; Unsorted configurations
   (setq org-format-latex-options
         (plist-put org-format-latex-options :background "Transparent"))
   (setq org-format-latex-options
@@ -19,11 +27,4 @@
   (setq org-list-allow-alphabetical t)
   (setq org-src-window-setup 'current-window)
   (setq org-use-speed-commands t)
-
-  ;; Toggle visibility of all code blocks by hitting C-c C-\
-  (defvar org-blocks-hidden nil)
-  (defun org-toggle-blocks ()
-    (interactive)
-    (if org-blocks-hidden (org-show-block-all) (org-hide-block-all))
-    (setq-local org-blocks-hidden (not org-blocks-hidden)))
-  (define-key org-mode-map (kbd "C-c C-\\") 'org-toggle-blocks))
+)
