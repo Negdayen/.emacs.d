b63e637bd856e84cbfb76cb6801656dfd860a7d1
Jacob Horrocks <jacob.horrocks@gmail.com>
2020-09-05 05:57:43 -0700

    python-mode: add SCons files to auto-mode-alist

--- horrocks/init.d/lang/python.el
+++ horrocks/init.d/lang/python.el
@@ -1,3 +1,6 @@
+(add-to-list 'auto-mode-alist
+             '("\\([Ss]conscript\\|[Ss]construct\\|SCsub\\)" . python-mode))
+
 (with-eval-after-load 'python
   (bind-key "C-c SPC" 'snake-region python-mode-map)
   (add-hook 'python-mode-hook 'subword-mode))
