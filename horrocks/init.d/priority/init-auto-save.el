7bd5fe4f0cd39365549de2200d1dd424821b4ed3
Jacob Horrocks <jacob.horrocks@gmail.com>
2020-09-05 18:35:35 -0700

    fix auto-save to save to ~/.emacs.d/auto-save

--- horrocks/init.d/priority/init-auto-save.el
+++ horrocks/init.d/priority/init-auto-save.el
@@ -3,11 +3,11 @@
 ;; I rather have auto-saves be stored in a single central location rather
 ;; than have them littered thoughout the filesystem. Remote files will still
 ;; be auto-saved to Emacs default location, as we're appending this rule.
-(defconst auto-save-dir (concat user-emacs-directory "/auto-save"))
+(defconst auto-save-dir (concat user-emacs-directory "auto-save"))
 (unless (file-exists-p auto-save-dir)
   (make-directory auto-save-dir))
 (add-to-list 'auto-save-file-name-transforms
-             (list ".*" auto-save-dir t)
+             (list "\\([^/]*/\\)*\\([^/]*\\)\\'" (concat auto-save-dir "/\\2") t)
              t)
 
 ;; Bump up the frequency of auto-saves.
