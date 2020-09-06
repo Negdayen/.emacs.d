e831fa3e403e2522543d86fd6b68574cef3a040c
Jacob Horrocks <jacob.horrocks@gmail.com>
2020-09-05 22:05:17 -0700

    ivy: avoid use-package idioms

--- horrocks/init.d/external/init-ivy.el
+++ horrocks/init.d/external/init-ivy.el
@@ -1,15 +1,15 @@
-(use-package ivy
-  :config
+(use-package ivy)
+(use-package ivy-hydra)
+
+(bind-key "M-n" 'ivy-restrict-to-matches ivy-minibuffer-map)
+
+(with-eval-after-load 'ivy
   (customize-setq
    ivy-count-format "(%d/%d) "
    ivy-extra-directories '()
    ivy-magic-tilde nil
    ivy-use-selectable-prompt t
    ivy-use-virtual-buffers nil)
-  (add-to-list 'ivy-ignore-buffers "\\*epc con")
-  :init
-  (ivy-mode)
-  :bind (:map ivy-minibuffer-map
-              ("M-n" . ivy-restrict-to-matches)))
+  (add-to-list 'ivy-ignore-buffers "\\*epc con"))
 
-(use-package ivy-hydra)
+(ivy-mode)
