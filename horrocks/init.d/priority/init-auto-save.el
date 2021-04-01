c1f300ca08b0568abbe091b69250115b6a4b913c
Jacob Horrocks <jacob.horrocks@gmail.com>
2021-03-31 21:39:48 -0700

    disable automatic saves
    Have very rarely found this feature useful; I just end up handling saves myself
    anyway.  It mostly just ends up annoying me with unexpected pauses.

--- horrocks/init.d/priority/init-auto-save.el
+++ horrocks/init.d/priority/init-auto-save.el
@@ -1,15 +1,1 @@
-(setq auto-save-default t)
-
-;; I rather have auto-saves be stored in a single central location rather
-;; than have them littered thoughout the filesystem. Remote files will still
-;; be auto-saved to Emacs default location, as we're appending this rule.
-(defconst auto-save-dir (concat user-emacs-directory "auto-save"))
-(unless (file-exists-p auto-save-dir)
-  (make-directory auto-save-dir))
-(add-to-list 'auto-save-file-name-transforms
-             (list "\\([^/]*/\\)*\\([^/]*\\)\\'" (concat auto-save-dir "/\\2") t)
-             t)
-
-;; Bump up the frequency of auto-saves.
-(setq auto-save-interval 100
-      auto-save-timeout 10)
+(setq auto-save-default nil)
