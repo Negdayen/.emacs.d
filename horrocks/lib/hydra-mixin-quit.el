925ca5394ed4b60dcba1cb0f252c915fb5001d4d
Jacob Horrocks <Jacob.Horrocks@gmail.com>
2021-07-23 05:41:11 -0700

    hydra-mixin-quit: bind hydra-repeat

--- horrocks/lib/hydra-mixin-quit.el
+++ horrocks/lib/hydra-mixin-quit.el
@@ -3,6 +3,7 @@
 
 (defhydra hydra-mixin-quit (:hint nil)
   ("C-g" nil "Quit" :exit t)
+  ("." hydra-repeat "Repeat")
   ("?"   toggle-hydra-is-helpful "Toggle hints")
   ("q"   nil "Quit" :exit t))
 
