56510ac47ccc0debc662bd78e5d5b09ca788320e
Jacob Horrocks <Jacob.Horrocks@gmail.com>
2021-07-23 06:02:10 -0700

    simple-x: add smarter commands to navigate to beginning of line

---
+++ horrocks/lib/simple-x.el
@@ -0,0 +1,14 @@
+(defun back-to-indentation* ()
+  "Move point to identation if not already there, else to beginning of line."
+  (interactive)
+  (if (= (save-excursion (back-to-indentation) (point))
+         (point))
+      (move-beginning-of-line nil)
+    (back-to-indentation)))
+
+(defun move-beginning-of-line* ()
+  "Move point to beginning of line if not already there, else to indentation."
+  (interactive)
+  (if (= (point) (point-at-bol))
+      (back-to-indentation)
+    (move-beginning-of-line nil)))
