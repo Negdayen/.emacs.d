d28f07f3b3e4940a1b22fde32300d3b8963e2594
Jacob Horrocks <Jacob.Horrocks@gmail.com>
2021-08-04 19:53:32 -0700

    emacs-lisp-mode: add elisp-x configurations

--- horrocks/init/major-mode/emacs-lisp.el
+++ horrocks/init/major-mode/emacs-lisp.el
@@ -1,3 +1,11 @@
 (customize-setq
  eval-expression-print-length nil
  eval-expression-print-level nil)
+
+;;;;;;;;;;;;;;
+;; elisp-x  ;;
+;;;;;;;;;;;;;;
+
+(require 'elisp-x)
+
+(elisp-x-default-configuration)
