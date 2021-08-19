3ce6f28b050c14e93da00eaba2d13f28c518efed
Jacob Horrocks <Jacob.Horrocks@gmail.com>
2021-08-19 04:52:31 -0700

    dired-tp: add command dired-kill-buffer-and-jump

--- horrocks/package/dired-tp.el
+++ horrocks/package/dired-tp.el
@@ -7,4 +7,11 @@
     "Dired-jump to directory:"
     (shell-command-to-list "env | grep -i DIR= | awk -F= '{ print $2 }'"))))
 
+(defun dired-kill-buffer-and-jump ()
+  "Kill the current buffer and jump to Dired."
+  (interactive)
+  (let ((buffer (current-buffer)))
+    (dired-jump)
+    (kill-buffer buffer)))
+
 (provide 'dired-tp)
