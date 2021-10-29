e83fa18995cb926b4bfac471a19cbd3f57448867
Jacob Horrocks <Jacob.Horrocks@gmail.com>
2021-10-29 06:20:39 -0700

    bookmark: initial configuration

---
+++ horrocks/init/builtin/bookmark.el
@@ -0,0 +1,6 @@
+(customize-setq
+ ;; save modifications to the bookmark file immediately
+ bookmark-save-flag 1
+ ;; if the bookmark file changes on disk, just reload without querying
+ bookmark-watch-bookmark-file 'silent
+)
