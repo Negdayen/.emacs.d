f7e789704f193b81c03094d90d85a9bf607bf7e5
Jacob Horrocks <Jacob.Horrocks@gmail.com>
2021-10-31 08:34:30 -0700

    browse-url: use qutebrowser directly and support opening new windows.

--- horrocks/init/builtin/browse-url.el
+++ horrocks/init/builtin/browse-url.el
@@ -1,11 +1,18 @@
-(cond
- ((eq system-type 'gnu/linux)
-  (defun browse-url-web-browser (url &optional new-window)
-    (shell-command (format "web-browser %s" url)))
-  (setq browse-url-browser-function 'browse-url-web-browser))
+(defun browse-url-edge (url &optional new-window)
+  (interactive (browse-url-interactive-arg "URL: "))
+  ;; TODO support opening in new window
+  (shell-command (concat "powershell start microsoft-edge:" url)))
 
- ((or (eq system-type 'cygwin)
-      (eq system-type 'windows-nt))
-  (defun browse-url-edge (url &optional new-window)
-    (shell-command (concat "powershell start microsoft-edge:" url)))
-  (setq browse-url-browser-function 'browse-url-edge)))
+(defun browse-url-qutebrowser (url &optional new-window)
+  (interactive (browse-url-interactive-arg "URL: "))
+  (let* ((use-new-window (or (browse-url-maybe-new-window new-window) browse-url-new-window-flag))
+         (use-new-window (if current-prefix-arg (not use-new-window) use-new-window)))
+    (shell-command
+     (format "qutebrowser %s %s" (if use-new-window "--target window" "") url))))
+
+(require 'cl)
+(case system-type
+  ((gnu/linux)
+   (customize-setq browse-url-browser-function 'browse-url-qutebrowser))
+  ((cygwin windows-nt)
+   (customize-setq browse-url-browser-function 'browse-url-edge)))
