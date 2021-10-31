9dc26d5ac4fb8120542813b6b674545896d9c854
Jacob Horrocks <Jacob.Horrocks@gmail.com>
2021-10-31 07:11:44 -0700

    prog-mode and text-mode:  add key bindings for global org links.

--- horrocks/init/builtin/prog-mode.el
+++ horrocks/init/builtin/prog-mode.el
@@ -3,3 +3,6 @@
 
 (add-hook 'prog-mode-hook 'auto-revert-mode)
 (add-hook 'prog-mode-hook 'whitespace-mode)
+
+(bind-key "C-c C-l" 'org-insert-link-global prog-mode-map)
+(bind-key "C-c C-o" 'org-open-at-point-global prog-mode-map)
--- horrocks/init/builtin/text-mode.el
+++ horrocks/init/builtin/text-mode.el
@@ -1,2 +1,5 @@
 (add-hook 'text-mode-hook 'auto-revert-mode)
 (add-hook 'text-mode-hook 'whitespace-mode)
+
+(bind-key "C-c C-l" 'org-insert-link-global text-mode-map)
+(bind-key "C-c C-o" 'org-open-at-point-global text-mode-map)
