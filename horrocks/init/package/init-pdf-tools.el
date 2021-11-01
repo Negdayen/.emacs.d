c099a9f8737e7e908cc24b81a8c411f22bc23f62
Jacob Horrocks <Jacob.Horrocks@gmail.com>
2021-11-01 07:18:36 -0700

    pdf-view-mode: automatically enable for .pdf files if not WSL.

--- horrocks/init/package/init-pdf-tools.el
+++ horrocks/init/package/init-pdf-tools.el
@@ -2,14 +2,16 @@
 (when (eq system-type 'gnu/linux)
   (use-package pdf-tools)
   (condition-case nil
-      ;; Installing will compile the necessary backend programs.
-      ;; Uninstall immediately after to not autoload pdf-view-mode because it has issues.
-      ;; Manually enabling pdf-view-mode after loading a .pdf file works fine.
-      ;;
+      ;; Installing will compile the necessary backend programs.  We uninstall immediately
+      ;; after to NOT unconditionally use pdf-view-mode on pdf files because it has issues
+      ;; under WSL; we conditionally enable it later.  (Manually enabling pdf-view-mode
+      ;; after loading a .pdf file works fine under WSL).
       ;; TODO See if issue persists if I bypass most of my configuration.
       (progn
         (pdf-tools-install)
-        (pdf-tools-uninstall))
+        (pdf-tools-uninstall)
+        (if (not (file-exists-p "/run/WSL"))
+            (add-to-list 'auto-mode-alist '("\\.pdf$" . pdf-view-mode))))
     (error (warn "pdf-tools-install failed"))))
 
 (customize-setq
